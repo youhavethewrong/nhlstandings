812ff59629d3ccbca76d40a934d901ad13d28e81
ESC <youhavethewrong@gmail.com>
2017-04-14 18:52:38 +0000

    Switch to httpkit.  Clean up formatting. Allow arbitrary casing of team name.

--- src/nhlstandings/core.clj
+++ src/nhlstandings/core.clj
@@ -1,5 +1,5 @@
 (ns nhlstandings.core
-  (:require [clj-http.client :as client]
+  (:require [org.httpkit.client :as httpkit]
             [clojure.data.json :as json])
   (:import [java.text SimpleDateFormat]
            [java.util Calendar])
@@ -7,7 +7,9 @@
 
 (def current-season "20162017")
 (def stats-api-root-url "https://statsapi.web.nhl.com/")
-(def stats-url (str stats-api-root-url "api/v1/standings?expand=standings.team&season=" current-season))
+(def stats-url
+  (str stats-api-root-url "api/v1/standings?expand=standings.team&season="
+       current-season))
 
 (defn days-from-now
   [n]
@@ -21,13 +23,15 @@
 (def a-week-from-now
   (days-from-now 7))
 
-(def schedule-url (str stats-api-root-url "api/v1/schedule?startDate=" yesterday "&endDate=" a-week-from-now))
+(def schedule-url
+  (str stats-api-root-url "api/v1/schedule?startDate=" yesterday
+       "&endDate=" a-week-from-now))
 
 ;;,schedule.linescore,schedule.broadcasts.all,schedule.ticket,schedule.game.content.media.epg,schedule.radioBroadcasts,schedule.game.seriesSummary,seriesSummary.series&leaderCategories=&leaderGameTypes=R&site=en_nhl&teamId=&gameType=&timecode=
 
 (defn check-standings
   [url]
-  (client/get url))
+  @(httpkit/get url {:user-agent "Aplix_SEGASATURN_browser/1.x (Japanese)"}))
 
 (defn find-records
   [data team-name]
@@ -35,7 +39,8 @@
    (fn [r]
      (let [teams (:teamRecords r)]
        (filter
-        #(= (:name %) team-name)
+        #(= (clojure.string/lower-case (:name %))
+            (clojure.string/lower-case team-name))
         (map
          (fn [team]
            {:name (:teamName (:team team))
@@ -49,19 +54,23 @@
          teams))))
      (:records data)))
 
+(defn delegate
+  [team]
+  (let [body (:body (check-standings stats-url))
+        _ (println body)
+        data (json/read-str body :key-fn keyword)
+        records (apply concat (find-records data team))]
+    (doseq [{:keys [name city record played scored against rank updated]} records]
+      (println "The" city name "have a current season record of"
+               (str (:wins record) " wins, " (:losses record)
+                    " losses, and " (:ot record) " overtime losses")
+               "out of" played "games played."
+               "\nThis season, they have scored" scored "goals and have had"
+               against "scored against them."
+               "\nThis information was last updated at" (str updated ".")))))
+
 (defn -main
   [& args]
   (if-not (first args)
     (println "Please supply a team name like 'Stars'")
-    (let [body (:body (check-standings stats-url))
-          data (json/read-str body :key-fn keyword)
-          records (apply concat (find-records data (first args)))]
-      (dorun
-       (map
-        (fn [{:keys [name city record played scored against rank updated]}]
-          (println "The" city name "have a current season record of"
-                   (str (:wins record) " wins, " (:losses record) " losses, and " (:ot record) " overtime losses")
-                   "out of" played "games played."
-                   "\nThis season, they have scored" scored "goals and have had" against "scored against them."
-                   "\nThis information was last updated at" (str updated ".")))
-        records)))))
+    (delegate (first args))))
